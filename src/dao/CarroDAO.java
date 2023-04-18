0fa7f5716dae0c065be64db8854182ce1e096394
Guilherme da Cunha Silveira <182120069@senacinformatica.br>
2023-04-17 21:49:44 -0300

    carroDAO

--- src/dao/CarroDAO.java
+++ src/dao/CarroDAO.java
@@ -8,33 +8,95 @@ import conexao.Conexao;
 import java.net.ConnectException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import model.Carro;
+import servicos.PessoaServicos;
+import servicos.ServicosFactory;
 
 /**
  *
  * @author 182120069
  */
 public class CarroDAO {
-    
-    public void cadastrarCarroDAO(Carro cVO){
+
+    public void cadastrarCarroDAO(Carro cVO) {
         try {
             Connection con = Conexao.getConexao();
             String sql = "insert into carros values (null,?,?,?,?,?,?,?,?,?)";
             PreparedStatement pst = con.prepareStatement(sql);
-            pst.setString(1,cVO.getPlaca());
-            pst.setString(2,cVO.getMarca());
-            pst.setString(3,cVO.getModelo());
-            pst.setInt(4,cVO.getAnoFab());
-            pst.setInt(5,cVO.getAnoMod());
-            pst.setString(6,cVO.getCor());
-            pst.setString(7,cVO.getTpCambio());
-            pst.setString(8,cVO.getCombustivel());
-            pst.setInt(9,cVO.getProprietario().getIdPessoa());
+            pst.setString(1, cVO.getPlaca());
+            pst.setString(2, cVO.getMarca());
+            pst.setString(3, cVO.getModelo());
+            pst.setInt(4, cVO.getAnoFab());
+            pst.setInt(5, cVO.getAnoMod());
+            pst.setString(6, cVO.getCor());
+            pst.setString(7, cVO.getTpCambio());
+            pst.setString(8, cVO.getCombustivel());
+            pst.setInt(9, cVO.getProprietario().getIdPessoa());
             pst.executeUpdate();
         } catch (SQLException e) {
-            System.out.println("Erro ao cadastrar Carro.\n"+e.getMessage());
+            System.out.println("Erro ao cadastrar Carro.\n" + e.getMessage());
         }
     }
-    
-}
+
+    public ArrayList<Carro> getCarros() {
+        ArrayList<Carro> carros = new ArrayList<>();
+        try {
+            Connection con = Conexao.getConexao();
+            String sql = "select c.*, p.cpf as cpf from carros c join pessoas p on c.proprietario =  p.idPessoa";
+            PreparedStatement pst = con.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                Carro c = new Carro();
+                c.setPlaca(rs.getString("placa"));
+                c.setMarca(rs.getString("marca"));
+                c.setModelo(rs.getString("modelo"));
+                c.setAnoFab(rs.getInt("anofab"));
+                c.setAnoMod(rs.getInt("anomod"));
+                c.setCor(rs.getString("cor"));
+                c.setTpCambio(rs.getString("tpcambio"));
+                c.setCombustivel(rs.getString("combustivel"));
+                PessoaServicos pessoaS = ServicosFactory.getPessoaServicos();
+                c.setProprietario(pessoaS.getPessoaByDoc(rs.getString("cpf")));
+                carros.add(c);
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao listar Carro.\n" + e.getMessage());
+        }
+        return carros;
+    }//fim getCarros
+
+    public Carro getCarroByDoc(String placa) {
+        Carro c = new Carro();
+        try {
+            Connection con = Conexao.getConexao();
+            String sql = "select c.*, p.cpf as cpf from carros c join pessoas p on c.proprietario =  p.idPessoa where placa = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, placa);
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                c.setPlaca(rs.getString("placa"));
+                c.setMarca(rs.getString("marca"));
+                c.setModelo(rs.getString("modelo"));
+                c.setAnoFab(rs.getInt("anofab"));
+                c.setAnoMod(rs.getInt("anomod"));
+                c.setCor(rs.getString("cor"));
+                c.setTpCambio(rs.getString("tpcambio"));
+                c.setCombustivel(rs.getString("combustivel"));
+                PessoaServicos pessoaS = ServicosFactory.getPessoaServicos();
+                c.setProprietario(pessoaS.getPessoaByDoc(rs.getString("cpf")));
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao buscar placa.\n" + e.getMessage());
+        }
+        return c;
+
+    }
+
+}// fim classe
